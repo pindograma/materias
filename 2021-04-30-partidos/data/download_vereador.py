58171a41551a41e357e941a33d5c823c7b02d92b
aspilt <antonio.piltcher@gmail.com>
2021-04-30 17:30:51 -0300

    add data for partidos em numeros final issue:

---
+++ 2021-04-30-partidos/data/download_vereador.py
@@ -0,0 +1,39 @@
+import requests
+import csv
+
+with open('cities.csv') as f:
+    oup = []
+
+    reader = csv.reader(f)
+    for line in reader:
+        print(line)
+
+        try:
+            data = requests.get('https://resultados.tse.jus.br/oficial/ele2020/divulgacao/oficial/426/dados-simplificados/{}/{}{}-c0013-e000426-r.json'.format(
+                line[1].lower(), line[1].lower(), line[0]
+            )).json()
+
+            for cand in data['cand']:
+                oup.append({
+                    'codigo_cidade_tse': line[0],
+                    'urnas_apuradas': data['pst'].strip(),
+                    'eleitorado': data['e'],
+                    'abstencoes': data['a'],
+                    'total_votos': data['tv'],
+                    'abstencoes_pct': data['pa'].strip(),
+                    'brancos_pct': data['pvb'].strip(),
+                    'nulos_pct': data['ptvn'].strip(),
+                    'nome': cand['nm'],
+                    'numero': cand['n'],
+                    'votos_validos': cand['vap'],
+                    'pct_votos_validos': cand['pvap'].strip(),
+                    'eleito': cand['st']
+                })
+        except:
+            print('Error! Continuing...')
+
+    keys = oup[0].keys()
+    with open('resultados.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(oup)
